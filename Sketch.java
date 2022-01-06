b8b4f892bfbdee7fedf16f1cd0a6ce538f220e0a
RomuelC <romuel.cuevas23@ycdsbk12.ca>
2022-01-05 22:50:17 -0500

    Final

--- Sketch.java
+++ Sketch.java
@@ -4,35 +4,34 @@ public class Sketch extends PApplet {
 
   // global variables 
 
-  // initalize size
+  // Initialize Size
   public float width = 1200f;
   public float height = 800f;
 
-  // scale factor 
+  // Scale Factor 
   public float wScaleF = width / 400f;
   public float hScaleF = height / 400f;
 
-  // circle size 
+  // Sun and Moon Size
+  public int sunSize = 150;
+  public int moonSize = 110;
+
+  // Star Size Scale
+  public float starScale = 0.35f;
+
+  // Circle Size 
   public float circleX = 0;
   public float circleY = 300 * hScaleF;
 
-  // day and night setting
+  // Day and Night setting
   public boolean day = true;
   public boolean night = false;
 
-  // timelapse speed
-  public int speed = 5;
-
-  // sun and moon size
-  public int sunSize = 90;
-  public int moonSize = 60;
-
-  // star size scale
-  public float starScale = 0.35f;
-
+  // Timelapse Speed
+  public int speed = 3;
 
   public void settings() {
-	  // put your size call here
+
     size((int)width, (int)height);
   }
 
@@ -41,44 +40,22 @@ public class Sketch extends PApplet {
   }
 
   public void draw() {
-    // clear out old frames
+
     if (day) {
-      background(115, 235, 255);
+      background(122, 229, 250);
     }
     else if (night) {
       background(0);
     }
 
-    // draw current frame based on state
     if (day) {
-      fill(250, 253, 15);
+      fill(250, 215, 122);
     }
     else if (night) {
       fill(255);
-      for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-          // draw star
-          pushMatrix();
-          translate((j * 73) * wScaleF, (i * 100) * hScaleF); // center of the star
-          beginShape();
-          vertex(0 * starScale, -50 * starScale);
-          vertex(14 * starScale, -20 * starScale);
-          vertex(47 * starScale, -15 * starScale);
-          vertex(23 * starScale, 7 * starScale);
-          vertex(29 * starScale, 40 * starScale);
-          vertex(0 * starScale, 25 * starScale);
-          vertex(-29 * starScale, 40 * starScale);
-          vertex(-23 * starScale, 7 * starScale);
-          vertex(-47 * starScale, -15 * starScale);
-          vertex(-14 * starScale, -20 * starScale);
-          translate(50 * wScaleF,0);
-          endShape(CLOSE);
-          popMatrix();
-        }
-      }
     }
 
-    // draw circle
+    // Circle Drawing
     if (day) {
       ellipse(circleX, circleY, sunSize, sunSize);
     }
@@ -86,11 +63,11 @@ public class Sketch extends PApplet {
       ellipse(circleX, circleY, moonSize, moonSize);
     }
 
-    // change state 
+    // Change State 
     circleX += speed;
     circleY = (float) ((0.0055 / wScaleF) * (Math.pow(circleX - width / 2, 2)) + (50 * hScaleF));
  
-    // reset state
+    // Reset State
     if(circleX > width + 50) {
       circleX = 0;
       circleY = 300 * hScaleF;
@@ -104,9 +81,12 @@ public class Sketch extends PApplet {
       }
     }
 
-    // ground 
-    fill(0, 255, 0); 
+    // Grass 
+    fill(23, 130, 29); 
     rect(0, 300 * hScaleF, width, height - 300);
-  }
 
+    // Dirt
+    fill(64, 42, 22);
+    rect(0, 320 * hScaleF, width, height - 300);
+  }
 }
